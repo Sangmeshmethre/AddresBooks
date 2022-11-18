9f20bf8120b1eb50368f9220798855c05f4334dd
Sangmesh Methre <ssmcmethre3080@gmail.com>
2022-11-18 23:26:29 +0530

    Use Lambda Experation

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using AddressBookSys.AddressBookSys;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSys
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System");
+
+            AddressBook newaddress = new AddressBook();
+            newaddress.ContactDetails();
+        }
+    }
+}
